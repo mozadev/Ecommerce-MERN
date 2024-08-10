f9061f6bb62a5ad62d1f94b8cdabcfcc65016867
mozadev <ceosmore@gmail.com>
2024-08-09 19:43:49 -0500

    update url backend prod

--- frontend/src/features/api.js
+++ frontend/src/features/api.js
@@ -6,9 +6,10 @@ const protocol = window.location.protocol;
 
 // Construir la URL dinámica
 // const url = `https://backend-production-670d.up.railway.app/api`;
+ const url = `ecommerce-mern-production-572d.up.railway.app/api`;
 
 // Cambiar la URL base de la API para el entorno local
-const url = `http://localhost:3001/api`;
+//const url = `http://localhost:3001/api`;
 
 // Exportar la URL
 export { url };
