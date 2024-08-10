ed61ab92ee16bf07ae9c662aa67ead412ac1a38a
mozadev <ceosmore@gmail.com>
2024-08-09 23:41:21 -0500

    config CORS in backed to accept frontend petitions

--- backend/index.js
+++ backend/index.js
@@ -17,13 +17,24 @@ const app = express();
 require("dotenv").config();
 
 
+// Configuración de CORS
+const corsOptions = {
+  origin: process.env.CLIENT_URL || 'http://localhost:3000', // Usa la URL de tu frontend en Railway o localhost para desarrollo
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 // Add these lines to increase the payload size limit
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 //app.use(express.json());
-app.use(cors());
+//app.use(cors());
 
+//rutas
 app.use("/api/register", register);
 app.use("/api/login", login);
 app.use("/api/stripe", stripe);
