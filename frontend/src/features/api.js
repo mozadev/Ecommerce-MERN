d80855c2af42a7084f4f58e4c31094ab8db4f168
mozadev <ceosmore@gmail.com>
2024-08-10 00:35:15 -0500

    config CORS in backed to accept frontend petitions

--- frontend/src/features/api.js
+++ frontend/src/features/api.js
@@ -6,7 +6,10 @@ const protocol = window.location.protocol;
 
 // Construir la URL dinámica
 // const url = `https://backend-production-670d.up.railway.app/api`;
-const url = `https://ecommerce-mern-production-572d.up.railway.app/api`;
+
+const url_backend = import.meta.env.VITE_API_URL;
+const url = `${url_backend}/api`;
+
 
 // Cambiar la URL base de la API para el entorno local
 //const url = `http://localhost:3001/api`;
